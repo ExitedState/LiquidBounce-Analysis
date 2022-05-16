9f9cf4eb7ffe514491ccaa4a1c40503ea4984c19
Phaiboon Jaradnaparatana <phaiboon760@gmail.com>
2022-05-16 09:19:04 +0700

    remove command - chatSyntax("say <message...>") - chat("Message was sent to the chat.")

--- src/main/java/net/ccbluex/liquidbounce/features/command/commands/SayCommand.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/command/commands/SayCommand.kt
@@ -15,9 +15,9 @@ class SayCommand : Command("say") {
     override fun execute(args: Array<String>) {
         if (args.size > 1) {
             mc.thePlayer!!.sendChatMessage(StringUtils.toCompleteString(args, 1))
-            chat("Message was sent to the chat.")
+//            chat("Message was sent to the chat.")
             return
         }
-        chatSyntax("say <message...>")
+//        chatSyntax("say <message...>")
     }
 }
