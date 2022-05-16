dc66d02a6f1f997dc0ad8db169dbd0658c3f18e0
Phaiboon Jaradnaparatana <phaiboon760@gmail.com>
2022-05-16 14:59:58 +0700

    - rename file for lucky package - change spammer message

--- src/main/java/net/ccbluex/luck/features/module/modules/misc/Spammer.java
+++ src/main/java/net/ccbluex/luck/features/module/modules/misc/Spammer.java
@@ -3,26 +3,26 @@
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
  * https://github.com/CCBlueX/LiquidBounce/
  */
-package net.ccbluex.liquidbounce.features.module.modules.misc;
+package net.ccbluex.luck.features.module.modules.misc;
 
-import net.ccbluex.liquidbounce.LiquidBounce;
-import net.ccbluex.liquidbounce.event.EventTarget;
-import net.ccbluex.liquidbounce.event.UpdateEvent;
-import net.ccbluex.liquidbounce.features.module.Module;
-import net.ccbluex.liquidbounce.features.module.ModuleCategory;
-import net.ccbluex.liquidbounce.features.module.ModuleInfo;
-import net.ccbluex.liquidbounce.utils.misc.RandomUtils;
-import net.ccbluex.liquidbounce.utils.timer.MSTimer;
-import net.ccbluex.liquidbounce.utils.timer.TimeUtils;
-import net.ccbluex.liquidbounce.value.BoolValue;
-import net.ccbluex.liquidbounce.value.IntegerValue;
-import net.ccbluex.liquidbounce.value.TextValue;
+import net.ccbluex.luck.event.EventTarget;
+import net.ccbluex.luck.event.UpdateEvent;
+import net.ccbluex.luck.features.module.Module;
+import net.ccbluex.luck.features.module.ModuleCategory;
+import net.ccbluex.luck.features.module.ModuleInfo;
+import net.ccbluex.luck.utils.misc.RandomUtils;
+import net.ccbluex.luck.utils.timer.MSTimer;
+import net.ccbluex.luck.utils.timer.TimeUtils;
+import net.ccbluex.luck.value.BoolValue;
+import net.ccbluex.luck.value.IntegerValue;
+import net.ccbluex.luck.value.TextValue;
 
 import java.util.Random;
 
 @ModuleInfo(name = "Spammer", description = "Spams the chat with a given message.", category = ModuleCategory.MISC)
 public class Spammer extends Module {
 
+    public static int cnt  =0;
     private final IntegerValue maxDelayValue = new IntegerValue("MaxDelay", 1000, 0, 5000) {
         @Override
         protected void onChanged(final Integer oldValue, final Integer newValue) {
@@ -46,7 +46,7 @@ public class Spammer extends Module {
         }
     };
 
-    private final TextValue messageValue = new TextValue("Message", LiquidBounce.CLIENT_NAME + " Client | liquidbounce(.net) | CCBlueX on yt");
+    private final TextValue messageValue = new TextValue("Message", "HelloWorld"+cnt++);
     private final BoolValue customValue = new BoolValue("Custom", false);
 
     private final MSTimer msTimer = new MSTimer();
