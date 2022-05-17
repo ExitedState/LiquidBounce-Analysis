13bdd50faa47fb5d9805027aec0deb959ddeae3c
Phaiboon Jaradnaparatana <phaiboon760@gmail.com>
2022-05-17 18:07:01 +0700

    undo last commit - add python script GUI to show key state

--- src/main/java/net/ccbluex/luck/injection/forge/mixins/gui/MixinGuiInGame.java
+++ src/main/java/net/ccbluex/luck/injection/forge/mixins/gui/MixinGuiInGame.java
@@ -32,48 +32,48 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @SideOnly(Side.CLIENT)
 public abstract class MixinGuiInGame {
 
-//    @Shadow
-//    protected abstract void renderHotbarItem(int index, int xPos, int yPos, float partialTicks, EntityPlayer player);
-//
-//    @Inject(method = "renderScoreboard", at = @At("HEAD"), cancellable = true)
-//    private void renderScoreboard(CallbackInfo callbackInfo) {
-//        if (LiquidBounce.moduleManager.getModule(HUD.class).getState() || NoScoreboard.INSTANCE.getState())
-//            callbackInfo.cancel();
-//    }
-//
-//    @Inject(method = "renderTooltip", at = @At("HEAD"), cancellable = true)
-//    private void renderTooltip(ScaledResolution sr, float partialTicks, CallbackInfo callbackInfo) {
-//        final HUD hud = (HUD) LiquidBounce.moduleManager.getModule(HUD.class);
-//
-//        if(Minecraft.getMinecraft().getRenderViewEntity() instanceof EntityPlayer && hud.getState() && hud.getBlackHotbarValue().get()) {
-//            EntityPlayer entityPlayer = (EntityPlayer) Minecraft.getMinecraft().getRenderViewEntity();
-//
-//            int middleScreen = sr.getScaledWidth() / 2;
-//
-//            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-//            GuiIngame.drawRect(middleScreen - 91, sr.getScaledHeight() - 24, middleScreen + 90, sr.getScaledHeight(), Integer.MIN_VALUE);
-//            GuiIngame.drawRect(middleScreen - 91 - 1 + entityPlayer.inventory.currentItem * 20 + 1, sr.getScaledHeight() - 24, middleScreen - 91 - 1 + entityPlayer.inventory.currentItem * 20 + 22, sr.getScaledHeight() - 22 - 1 + 24, Integer.MAX_VALUE);
-//
-//            GlStateManager.enableRescaleNormal();
-//            GL11.glEnable(GL11.GL_BLEND);
-//            GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-//            RenderHelper.enableGUIStandardItemLighting();
-//
-//            for(int j = 0; j < 9; ++j) {
-//                int k = sr.getScaledWidth() / 2 - 90 + j * 20 + 2;
-//                int l = sr.getScaledHeight() - 16 - 3;
-//                this.renderHotbarItem(j, k, l, partialTicks, entityPlayer);
-//            }
-//
-//            RenderHelper.disableStandardItemLighting();
-//            GlStateManager.disableRescaleNormal();
-//            GlStateManager.disableBlend();
-//
-//            LiquidBounce.eventManager.callEvent(new Render2DEvent(partialTicks));
-//            AWTFontRenderer.Companion.garbageCollectionTick();
-//            callbackInfo.cancel();
-//        }
-//    }
+    @Shadow
+    protected abstract void renderHotbarItem(int index, int xPos, int yPos, float partialTicks, EntityPlayer player);
+
+    @Inject(method = "renderScoreboard", at = @At("HEAD"), cancellable = true)
+    private void renderScoreboard(CallbackInfo callbackInfo) {
+        if (LiquidBounce.moduleManager.getModule(HUD.class).getState() || NoScoreboard.INSTANCE.getState())
+            callbackInfo.cancel();
+    }
+
+    @Inject(method = "renderTooltip", at = @At("HEAD"), cancellable = true)
+    private void renderTooltip(ScaledResolution sr, float partialTicks, CallbackInfo callbackInfo) {
+        final HUD hud = (HUD) LiquidBounce.moduleManager.getModule(HUD.class);
+
+        if(Minecraft.getMinecraft().getRenderViewEntity() instanceof EntityPlayer && hud.getState() && hud.getBlackHotbarValue().get()) {
+            EntityPlayer entityPlayer = (EntityPlayer) Minecraft.getMinecraft().getRenderViewEntity();
+
+            int middleScreen = sr.getScaledWidth() / 2;
+
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+            GuiIngame.drawRect(middleScreen - 91, sr.getScaledHeight() - 24, middleScreen + 90, sr.getScaledHeight(), Integer.MIN_VALUE);
+            GuiIngame.drawRect(middleScreen - 91 - 1 + entityPlayer.inventory.currentItem * 20 + 1, sr.getScaledHeight() - 24, middleScreen - 91 - 1 + entityPlayer.inventory.currentItem * 20 + 22, sr.getScaledHeight() - 22 - 1 + 24, Integer.MAX_VALUE);
+
+            GlStateManager.enableRescaleNormal();
+            GL11.glEnable(GL11.GL_BLEND);
+            GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+            RenderHelper.enableGUIStandardItemLighting();
+
+            for(int j = 0; j < 9; ++j) {
+                int k = sr.getScaledWidth() / 2 - 90 + j * 20 + 2;
+                int l = sr.getScaledHeight() - 16 - 3;
+                this.renderHotbarItem(j, k, l, partialTicks, entityPlayer);
+            }
+
+            RenderHelper.disableStandardItemLighting();
+            GlStateManager.disableRescaleNormal();
+            GlStateManager.disableBlend();
+
+            LiquidBounce.eventManager.callEvent(new Render2DEvent(partialTicks));
+            AWTFontRenderer.Companion.garbageCollectionTick();
+            callbackInfo.cancel();
+        }
+    }
 
 
     @Inject(method = "renderTooltip", at = @At("RETURN"))
